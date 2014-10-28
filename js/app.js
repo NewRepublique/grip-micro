b625587882a41dbb8b651a1bfb13a0830a4fedc2
Jackson Alsop <jjalsop@gmail.com>
2014-10-29 09:41:01 +1100

    basic copy added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+windowSize = $(window).height();
+
+$('section').css('height', windowSize + 'px')
