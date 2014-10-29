b91e234f92ade712adbca5258528a067720930de
Jackson Alsop <jjalsop@gmail.com>
2014-10-29 11:30:50 +1100

    finalised scroll structure

--- js/app.js
+++ js/app.js
@@ -2,6 +2,51 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-windowSize = $(window).height();
+$(document).ready(function(){
+  $(".scroll").click(function(event){
+    //prevent the default action for the click event
+    event.preventDefault();
 
-$('section').css('height', windowSize + 'px')
+    //get the full url - like mysitecom/index.htm#home
+    var full_url = this.href;
+
+    //split the url by # and get the anchor target name - home in mysitecom/index.htm#home
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+
+    //get the top offset of the target anchor
+    var target_offset = $("#"+trgt).offset(); 
+    var target_top = target_offset.top;
+
+    //goto that anchor by setting the body scroll top to anchor top
+    $('html, body').animate({scrollTop:target_top}, 850);
+    var chosenDiv = $("#"+trgt)
+
+    
+  });
+});
+
+$("#grip").onepage_scroll({
+   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+   easing: "ease-in-out",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+   animationTime: 600,             // AnimationTime let you define how long each section takes to animate
+   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+   keyboard: true,                  // You can activate the keyboard controls
+   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                    // the browser's width is less than 600, the fallback will kick in.
+   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+});
+
+$('.nextUp').click(function(){
+	$("#grip").moveDown();
+})
+
+$('.getBook').click(function(){
+	 $("#grip").moveTo(8);
+})
